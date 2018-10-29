8d88d556e5fc2e76ae6c2b2c46b4feb03a955526
Meynet Léo <44263847+Neexos@users.noreply.github.com>
2018-10-29 18:57:38 +0100

    Add files via upload

--- TP3/couleur_compteur.c
+++ TP3/couleur_compteur.c
@@ -6,14 +6,34 @@ int main(int argc, char **argv){
         int green;
         int blue;
         int alpha;
+        int count;
+        
     };
     
+    struct Rgba color;
+    
     char end[3];
+    char fin[3] = "FIN";
+    int i = 0;
+    
     
     printf("écrivez des couleurs en hexadecimal au format RGBA\n");
-    struct Rgba *couleurs = fgets(end, 3, stdin);
     
-    for(int i=0; i<=9; i++){
-        printf("couleur %i: %hhx %hhx %hhx %hhx\n", i, couleurs[i].red, couleurs[i].green, couleurs[i].blue, couleurs[i].alpha);
+
+    
+    while(1){
+        char couleur[30];
+        fgets(couleur, 30, stdin);
+        if(couleur[0]!='F' && couleur[1]!='I' && couleur[3]!='N'){
+            print_color("color.txt", couleur);
+            sscanf(couleur, "%x %x %x %x", &color.red, &color.green, &color.blue, &color.alpha);
+        }
+        else{
+             break;
+        }
+        printf("%x %x %x %x\n", color.red, color.green, color.blue, color.alpha);
     }
+    
+    
+    return 0;
 }
--- TP3/couleur_compteur.h
+++ TP3/couleur_compteur.h
@@ -6,3 +6,10 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
+char print_color(char *filename, char *message){
+    int fd,size;
+    
+    fd = open(filename, O_CREAT|O_WRONLY|O_APPEND);
+    size = write(fd, message, strlen(message));
+    close(fd);
+}
